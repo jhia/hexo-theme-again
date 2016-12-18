2028a1800a0b51a46187b3629215b0ecd66472f6
Jhinel Arcaya <proyectoamicoro@gmail.com>
2016-12-18 11:51:42 -0400

    First commit

--- source/js/app.js
+++ source/js/app.js
@@ -1,29 +1,11 @@
-var app = (function () {
-  var loadBanner = function () {
-    var bannerSmall = document.querySelector('.banner-small')
-
-    // can't just listen the bannerSmall's load event
-    // but why ?
-    var img = new Image()
-    img.src = bannerSmall.src
-    img.addEventListener('load', function (e) {
-      bannerSmall.classList.add('loaded')
-    }, false)
-
-    var bannerLarge = new Image()
-    bannerLarge.src = bannerSmall.dataset.src
-    bannerLarge.addEventListener('load', function (e) {
-      e.target.classList.add('loaded')
-    }, false)
-
-    bannerSmall.parentNode.appendChild(bannerLarge)
+(() => {
+  function changeAvatar(avatar) {
+    avatar.to(avatar._curIconId === 'cup' ? 'blockie' : 'cup');
   }
-
-  return {
-    loadBanner: loadBanner
+  if(document.querySelector('.banner')) {
+    const avatar = new SVGMorpheus('#avatar', { iconId: 'blockie', rotation: 'random'});
+    setInterval(changeAvatar.bind(this, avatar), 3000);  
+  } else {
+    document.querySelector('.app-nav').className += ' green';
   }
-})()
-
-window.addEventListener('load', function (e) {
-  app.loadBanner()
-}, false)
+})();
